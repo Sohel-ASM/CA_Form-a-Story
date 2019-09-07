e444954f7a057679f627e877cafb5ff758e81713
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-06 22:25:14 -0400

    trims and capitalizes the names

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,11 @@
 // Grab values from the submitted form in the URL
 const words = new URLSearchParams(window.location.search);
+
+// Cleans up and capitalizes the names of the animals
+function cleanAndCap(str) {
+    if (!str) {
+        return null;
+    }
+    let temp = str.trim();
+    return temp[0].toUpperCase() + temp.substring(1);
+}
