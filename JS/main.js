9ab4ca8645bde582318ed57f9bc372ff3403690f
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-06 22:31:57 -0400

    assigning the variables with values

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,20 @@ function cleanAndCap(str) {
     let temp = str.trim();
     return temp[0].toUpperCase() + temp.substring(1);
 }
+
+// Assigning the variables with values used in the story
+const firstAnimal = cleanAndCap(words.get('animal-1'));
+const secondAnimal = cleanAndCap(words.get('animal-2'));
+
+const answer = cleanAndCap(words.get('answer'));
+const conjunction = answer === 'Yes' ? 'and' : 'but';
+
+const speed = words.get('speed');
+const adj1 = words.get('adj-1');
+
+const thirdAnimal = cleanAndCap(words.get('animal-3'));
+const quote = words.get('quote');
+
+const verb1 = words.get('verb-1');
+const num1 = words.get('num-1');
+const message = words.get('message');
